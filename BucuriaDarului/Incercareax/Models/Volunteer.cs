7d1c1f68a816d0c1f4e3b0b0057413a33879565d
Fleischman Krisztian <kriky_sk8@yahoo.com>
2019-05-30 09:56:49 +0300

    bd/contract ep done

--- BucuriaDarului/Incercareax/Models/Volunteer.cs
+++ BucuriaDarului/Incercareax/Models/Volunteer.cs
@@ -4,6 +4,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using VolCommon;
 
+
 namespace Incercareax.Models
 {
 
@@ -11,7 +12,41 @@ namespace Incercareax.Models
     {
         [BsonId]
         public ObjectId VolunteerID { get; set; }
-      
+
+
+        public static int Volbd(Volunteer vol)
+        {
+            int voldays;
+            {
+                voldays = (vol.Birthdate.Month - 1) * 30 + vol.Birthdate.Day;
+
+            }
+            return voldays;
+        }
+
+        public static int Nowdate()
+        {
+            string todaydate = DateTime.Today.ToString("dd-MM-yyyy");
+            string[] dates = todaydate.Split('-');
+            int Day = Convert.ToInt16(dates[0]);
+            int Month = Convert.ToInt16(dates[1]);
+            int Year = Convert.ToInt16(dates[2]);
+            /*Day = 28;
+            Month = 12;
+            Year = 2019;*/
+            Day = (Month - 1) * 30 + Day;
+            return Day;
+        }
+        public static int Volexp(Volunteer vol)
+        {
+            int volexp;
+            {
+                volexp = (vol.Contract.ExpirationDate.Month - 1) * 30 + vol.Contract.ExpirationDate.Day;
+
+            }
+            return volexp;
+        }
+
     }
 }
 
